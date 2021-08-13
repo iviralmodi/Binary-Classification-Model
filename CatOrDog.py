456f2378c06b8e6990cd67d18b743fbc2b2b1cd5
iviralmodi <46816277+iviralmodi@users.noreply.github.com>
2021-08-13 08:23:10 +0530

    Add files via upload

---
+++ CatOrDog.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D
+
+image_gen = ImageDataGenerator(rotation_range=30, # rotate the image 30 degrees
+                               width_shift_range=0.1, # Shift the pic width by a max of 10%
+                               height_shift_range=0.1, # Shift the pic height by a max of 10%
+                               rescale=1/255, # Rescale the image by normalzing it.
+                               shear_range=0.2, # Shear means cutting away part of the image (max 20%)
+                               zoom_range=0.2, # Zoom in by 20% max
+                               horizontal_flip=True, # Allo horizontal flipping
+                               fill_mode='nearest' # Fill in missing pixels with the nearest filled value
+                              )
+
+
+image_shape = (150,150,3)
+
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(3,3),input_shape=(150,150,3), activation='relu',))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(filters=64, kernel_size=(3,3),input_shape=(150,150,3), activation='relu',))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(filters=64, kernel_size=(3,3),input_shape=(150,150,3), activation='relu',))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
+model.add(Flatten())
+
+
+model.add(Dense(128))
+model.add(Activation('relu'))
+
+model.add(Dropout(0.5))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+batch_size = 16
+
+train_image_gen = image_gen.flow_from_directory('CATS_DOGS/train',
+                                               target_size=image_shape[:2],
+                                               batch_size=batch_size,
+                                               class_mode='binary')
+
+test_image_gen = image_gen.flow_from_directory('CATS_DOGS/test',
+                                               target_size=image_shape[:2],
+                                               batch_size=batch_size,
+                                               class_mode='binary')
+
+results = model.fit_generator(train_image_gen,epochs=100,
+                              steps_per_epoch=150,
+                              validation_data=test_image_gen,
+                             validation_steps=12)
+ 
+# train_image_gen.class_indices
+
+dog_file = 'CATS_DOGS/train/Dog/2.jpg'
+
+dog_img = image.load_img(dog_file, target_size=(150, 150))
+
+dog_img = image.img_to_array(dog_img)
+
+dog_img = np.expand_dims(dog_img, axis=0)
+dog_img = dog_img/255
+
+prediction_prob = model.predict(dog_img)
+
+# Output prediction
+print(f'Probability that image is a dog is: {prediction_prob} ')
